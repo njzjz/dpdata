00a94fcc42476e350543cdf9e84ca0a2a005a4f9
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2022-06-08 17:02:30 -0400

    add minimize

--- dpdata/driver.py
+++ dpdata/driver.py
@@ -1,8 +1,13 @@
 """Driver plugin system."""
-from typing import Callable
+from typing import Callable, TYPE_CHECKING
+
 from .plugin import Plugin
 from abc import ABC, abstractmethod
 
+import dpdata
+
+if TYPE_CHECKING:
+    import ase
 
 class Driver(ABC):
     """The base class for a driver plugin. A driver can
@@ -78,3 +83,61 @@ class Driver(ABC):
             labeled data with energies and forces
         """
         return NotImplemented
+
+    def minimize(self, data: dict) -> dict:
+        """Minimize the geometry.
+
+        If not implemented, this method calls ASE to minimize.
+
+        Parameters
+        ----------
+        data : dict
+            data with coordinates and atom types
+        
+        Returns
+        -------
+        dict
+            labeled data with minimized coordinates, energies, and forces
+        """
+        from ase.optimize import BFGS
+        
+        system = dpdata.System(data=data)
+        # list[Atoms]
+        structures = system.to_ase_structure()
+        labeled_system = dpdata.LabeledSystem()
+        for atoms in structures:
+            atoms.calc = self.ase_calculator
+            dyn = BFGS(atoms)
+            dyn.run(fmax=5e-3)
+            ls = dpdata.LabeledSystem(atoms, fmt="ase/structure")
+            labeled_system.append(ls)
+        return labeled_system.data
+
+    @property
+    def ase_calculator(self) -> "ase.calculators.calculator.Calculator":
+        """Returns an ase calculator based on this driver."""
+        from .ase_calculator import DPDataCalculator
+        return DPDataCalculator(self)
+
+
+@Driver.register("hybrid")
+class HybridDriver(Driver):
+    def __init__(self, drivers: dict) -> None:
+        self.drivers = []
+        for key, driver in drivers.items():
+            if isinstance(driver, Driver):
+                self.drivers.append(driver)
+            elif isinstance(driver, dict):
+                self.drivers.append(Driver.get_driver(key)(**driver))
+            else:
+                raise TypeError("driver should be Driver or dict")
+
+    def label(self, data: dict) -> dict:
+        for ii, driver in enumerate(self.drivers):
+            lb_data = driver.label(data)
+            if ii == 0:
+                labeled_data = lb_data
+            else:
+                labeled_data['energies'] += lb_data ['energies']
+                labeled_data['forces'] += lb_data ['forces']
+        return labeled_data
